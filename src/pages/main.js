a67b19ba0c90a558605c5bce3bb8292684b63c36
Thiago Emmerich <dev2@brametec.com.br>
2019-10-17 08:13:07 -0300

    Added FlatList on Main Page

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,19 +1,50 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import api from '../services/api';
 
-import {View, Text} from 'react-native';
+import { View, Text, FlatList, TouchableOpacity } from 'react-native';
 
 export default class Main extends Component {
-    
+
     static navigationOptions = {
         title: "FirstApp"
     };
-    
+
+    state = {
+        docs: [],
+    }
+
+    componentDidMount() {
+        this.loadProducts();
+    }
+
+    loadProducts = async () => {
+        const response = await api.get('/products');
+
+        const { docs } = response.data;
+
+        this.setState({ docs });
+    }
+
+    renderItem = ({ item }) => (
+        <View>
+            <Text>{item.title}</Text>
+            <Text>{item.description}</Text>
+
+            <TouchableOpacity onPress={() => { }}>
+                <Text>Acessar</Text>
+            </TouchableOpacity>
+        </View>
+    )
+
     render() {
-        return(
-            <View>
-                <Text>
-                    Eu sou o Batman!
-                </Text>
+        return (
+            <View style={StyleSheet.container}>
+                <FlatList
+                    contentContainerStyle={style.list}
+                    data={this.state.docs}
+                    keyExtractor={item => item._id}
+                    renderItem={this.renderItem}
+                />
             </View>
         );
     }
